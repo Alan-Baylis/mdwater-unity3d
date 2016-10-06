3f5f3ec6b9c56b98333329ef4db8db6b88068105
ahpho <kuangsihao@gmail.com>
2016-10-06 10:31:03 +0800

    普通平面mesh创建成功

---
+++ Assets/MMWater/Scripts/MMMeshCreator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MMMeshCreator
+{
+    public static Mesh CreateWaterMesh()
+    {
+        float width = 2;
+        float height = 2;
+
+        Mesh mesh = new Mesh();
+        mesh.name = "ScriptedMesh";
+        mesh.vertices = new Vector3[] {
+            new Vector3(-width,0, -height),
+            new Vector3(width, 0, -height),
+            new Vector3(width, 0, height),
+            new Vector3(-width,0, height)
+        };
+        mesh.uv = new Vector2[] {
+            new Vector2 (0, 0),
+            new Vector2 (0, 1),
+            new Vector2 (1, 1),
+            new Vector2 (1, 0)
+        };
+        mesh.triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+}
---
+++ Assets/MMWater/Scripts/MMWater.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MMWater : MonoBehaviour {
+
+    void Awake()
+    {
+        InitializeWaterMeshes();
+    }
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    private void InitializeWaterMeshes()
+    {
+        GameObject waterMesh = transform.Find("WaterMesh").gameObject;
+
+        MeshFilter meshFilter = waterMesh.GetComponent<MeshFilter>();
+        meshFilter.mesh.Clear();
+        Mesh newMesh = CreateMesh(2);
+        meshFilter.mesh = newMesh;
+    }
+
+    private Mesh CreateMesh(float sideLen)
+    {
+        float width = sideLen;
+        float height = sideLen;
+
+        Mesh mesh = new Mesh();
+        mesh.name = "WaterMesh";
+        mesh.vertices = new Vector3[] {
+            new Vector3(-width, 0, -height),
+            new Vector3( width, 0, -height),
+            new Vector3( width, 0,  height),
+            new Vector3(-width, 0,  height)
+        };
+        mesh.uv = new Vector2[] {
+            new Vector2 (0, 0),
+            new Vector2 (0, 1),
+            new Vector2 (1, 1),
+            new Vector2 (1, 0)
+        };
+        mesh.triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    [ContextMenu("Test")]
+    void Test()
+    {
+    }
+}
